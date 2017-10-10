d7a6c83468f0f6691e43f3af6bcc7d0018c390fb
Oscar Abad Folgueira <oscar@dinapyme.com>
2017-10-10 22:44:36 +0200

    Version 0.1

---
+++ wc-free-text-in-products-with-price-0/readme.txt
@@ -0,0 +1,41 @@
+=== WC Free Text in Product Witch Price 0 ===
+
+Contributors: oabadfol
+Tags: free, text, price, woocommerce, product, shop page, categories, change text, set free, price 0, free product
+Requires at least: 4.5.0
+Tested up to: 4.8.2
+
+License: GPLv3 or Later License
+URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+This plugin lets you show the text "Free!" in products with price 0
+
+== Description ==
+
+
+
+
+
+== Installation ==
+
+
+== Frequently Asked Questions ==
+
+= Q: How do I use akdf alfljkdajlfjak jfkja fjasklj ? =
+A: dslfkajk fdljk
+
+
+
+
+== Screenshots ==
+1. Settings
+2. Shop page
+3. Single product page
+
+== Changelog ==
+
+= 1.0 =
+* first realease
+
+= 1.1 =
+* added support for bookable products
---
+++ wc-free-text-in-products-with-price-0/wc-free-text-in-products-with-price-0.php
@@ -0,0 +1,66 @@
+<?php
+/**     
+ * Plugin Name: WC Free Text in Products With Price 0
+ * Plugin URI: https://www.valhallapress.com/plugins/wc-free-text-in-products-with-price-0/
+ * Version: 0.1
+ * Description: This plugin lets you show the text "Free!" in products with price 0.
+ * Author: oabadfol
+ * Tested up to: 4.8.2
+ * Author URI: http://www.valhallapress.com
+ * Text Domain: valh-wcftipwp0
+ * Domain Path: /languages/
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+// Go away!
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Check if WooCommerce is installed and active
+ **/
+if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ), true ) ) {
+
+	// Localization
+	load_plugin_textdomain( 'woocustomizer', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	
+	/**
+	 * UP_Woocustomizer
+	 */
+	 
+	if ( ! class_exists( 'VALH_Wcftipwp0' ) ) {
+
+		final class VALH_Wcftipwp0 {
+
+			/**
+			 * The constructor!
+			 */
+			public function __construct() {
+
+				add_filter( 'woocommerce_get_price_html', array( $this, 'valh_wc_free_text_in_products_with_price_0'), 10, 2 );
+			}
+
+
+			public function valh_wc_free_text_in_products_with_price_0( $price, $product ) {
+				if ( $product->get_price() == 0 ) {
+					if ( $product->is_on_sale() && $product->get_regular_price() ) {
+						$regular_price = wc_get_price_to_display( $product, array( 'qty' => 1, 'price' => $product->get_regular_price() ) );
+
+						$price = wc_format_price_range( $regular_price, __( 'Free!', 'woocommerce' ) );
+					} else {
+						$price = '<span class="amount">' . __( 'Free!', 'woocommerce' ) . '</span>';
+					}
+				}
+
+				return $price;
+			} // End of main function
+
+		} // End of VALH_Wcftipwp0 class
+
+		$valh_wcftipwp0 = new VALH_Wcftipwp0();
+
+	} // End of checking if class exists
+
+} // End of checking if Woocommerce is installed and active
